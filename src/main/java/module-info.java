5e3c838bbbc20c8692a3fadf97955f9eba71e63f
MathisPipart <117630102+MathisPipart@users.noreply.github.com>
2024-11-24 19:14:57 +0100

    Start Server

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.mail_server {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.mail_server to javafx.fxml;
+    exports org.example.mail_server;
+}
