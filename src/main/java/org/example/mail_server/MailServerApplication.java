b84af985eb9a3fdfcdf01294228e4e340b3840f9
MathisPipart <117630102+MathisPipart@users.noreply.github.com>
2024-11-25 01:43:04 +0100

    Server Connected to Client v2

--- src/main/java/org/example/mail_server/MailServerApplication.java
+++ src/main/java/org/example/mail_server/MailServerApplication.java
@@ -27,45 +27,13 @@ public class MailServerApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MailServerApplication.class.getResource("mailServer-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Mail Server");
         stage.setScene(scene);
         stage.show();
 
-        startServer();
+        ConnexionClient connexionClient = new ConnexionClient();
+        connexionClient.startServer();
     }
 
-    public void startServer() {
-        System.out.println("Finestra del socket server");
-        try {
-            ServerSocket s = new ServerSocket(8189);
-            Socket incoming = s.accept( );
-            try {
-                ObjectInputStream inStream = new ObjectInputStream(incoming.getInputStream());
-                OutputStream outStream = incoming.getOutputStream();
 
-                PrintWriter out = new PrintWriter(outStream, true);
-
-                out.println( "Hello! Waiting for data." );
-
-                // echo client input
-                Vector<Date> v = null;
-                try {
-                    v = ((Vector<Date>)inStream.readObject());
-                } catch (ClassNotFoundException e) {System.out.println(e.getMessage());}
-
-                if (v!=null)
-                    for (int i=0; i<v.size(); i++) {
-                        Date date = v.get(i);
-                        System.out.println("Echo: " + date);
-                        out.println("Echo: " + date);
-                    }
-            }
-            finally {
-                incoming.close();
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
