c108dcf1f62c06a1674dd2cd6581c39cfde3b565
MathisPipart <117630102+MathisPipart@users.noreply.github.com>
2024-12-24 16:05:58 +0100

    code cleaning v2

--- src/main/java/org/example/MailServerApplication.java
+++ src/main/java/org/example/MailServerApplication.java
@@ -3,7 +3,6 @@ package org.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.example.controller.MailServerController;
 
